7d097fa8fb2011689ad0cf0b2fbae8d012498410
Antonio Jose Martin Vargas <a.martin.vargas@avanade.com>
2022-11-09 12:45:50 +0100

    Primeros Test sobre el controlador

---
+++ PlaylistInfo.Test/CancionesControllerTest.cs
@@ -0,0 +1,125 @@
+﻿using AutoMapper;
+using Beca.PlaylistInfo.API.Controllers;
+using Beca.PlaylistInfo.API.Entities;
+using Beca.PlaylistInfo.API.Models;
+using Beca.PlaylistInfo.API.Profiles;
+using Beca.PlaylistInfo.API.Services;
+using Castle.Core.Logging;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaylistInfo.Test
+{
+    public class CancionesControllerTest
+    {
+        private readonly ILogger<CancionesController> _logger;
+        private readonly CancionesController _playlistController;
+        private readonly Mapper _mapper;
+
+        public CancionesControllerTest()
+        {
+            //Arrage
+            var playlistMock = new Mock<IPlaylistRepository>();
+            playlistMock
+                .Setup(m => m.GetPlaylistsAsync(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<int>()))
+                .ReturnsAsync(new List<Playlist>
+                {new Playlist("linkin park"){Id= 1, Descripcion = "buena lkista"},
+                new Playlist("skillet"){Id= 2,Descripcion = "buena play"},
+                new Playlist("solence"){Id=3, Descripcion = "canerita"}
+                });
+            playlistMock
+                .Setup(m => m.PlaylistExistAsync(It.IsAny<int>()))
+                .ReturnsAsync(true);
+            playlistMock
+                .Setup(m => m.GetCancionesAsync(It.IsAny<int>()))
+                .ReturnsAsync(new List<Cancion>
+                {
+                    new Cancion("in the end"){Descripcion="best music"},
+                    new Cancion("in the end"){Descripcion="best music"}
+                });
+            playlistMock
+                .Setup(m => m.GetCancionAsync(It.IsAny<int>(), It.IsAny<int>()))
+                .ReturnsAsync(
+                    new Cancion("in the end"){Descripcion="best music"}
+                 );
+            playlistMock
+                .Setup(m => m.AddCancionAsync(It.IsAny<int>(), It.IsAny<Cancion>()));
+            playlistMock
+                .Setup(m => m.SavesChangesAsync())
+                .ReturnsAsync(
+                    true
+                 );
+
+
+            var mapperConfi = new MapperConfiguration(
+                cfg => cfg.AddProfile<CancionProfile>());
+            var mapper = new Mapper(mapperConfi);
+            _mapper = mapper;
+            var logger = Mock.Of<ILogger<CancionesController>>();
+            _logger = logger;
+            _playlistController = new CancionesController(logger, playlistMock.Object, mapper);
+
+        }
+
+        [Fact]
+        public async Task GetCanciones_Test()
+        {
+            //Arrage
+
+            //Act
+
+            var result = await _playlistController.GetCanciones(1);
+
+            //Assert
+            var actionResult = Assert.IsType<ActionResult<IEnumerable<CancionDto>>>(result);
+            var okObject = Assert.IsType<OkObjectResult>(actionResult.Result);
+            var dtos = Assert.IsAssignableFrom<IEnumerable<CancionDto>>(okObject.Value);
+            Assert.Equal(2, dtos.Count());
+
+
+        }
+
+        [Fact]
+        public async Task GetCancion_Test()
+        {
+            //Arrage
+
+            //Act
+
+            var result = await _playlistController.GetCancion(2,1);
+
+            //Assert
+            var actionResult = Assert.IsType<ActionResult<CancionDto>>(result);
+            var okObject = Assert.IsType<OkObjectResult>(actionResult.Result);
+            var dtos = Assert.IsAssignableFrom<CancionDto>(okObject.Value);
+            Assert.NotNull(dtos);   
+
+        }
+
+        [Fact]
+        public async Task CreateCancion_Test()
+        {
+            //Arrage
+
+            //Act
+            CancionCreationDto cancion = new CancionCreationDto()
+            {
+                Nombre="ok",Descripcion = "prueba"
+            } ;
+
+            var result = await _playlistController.CreateCancion(2, cancion);
+
+            //Assert
+            var actionResult = Assert.IsType<ActionResult<CancionDto>>(result);
+            var okObject = Assert.IsType<CreatedAtRouteResult>(actionResult.Result);
+
+        }
+
+    }
+}
---
+++ PlaylistInfo.Test/PlaylistControllerTest.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using Beca.PlaylistInfo.API.Services;
+using Beca.PlaylistInfo.API.Entities;
+using Beca.PlaylistInfo.API.Controllers;
+using AutoMapper;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PlaylistInfo.Test
+{
+    public class PlaylistControllerTest
+    {
+        private readonly PlaylistsController _playlistController;
+        public PlaylistControllerTest()
+        {
+            //Arrage
+            var playlistMock = new Mock<IPlaylistRepository>();
+            playlistMock
+                .Setup(m => m.GetPlaylistsAsync(It.IsAny<string>(),It.IsAny<int>(),It.IsAny<int>()))
+                .ReturnsAsync( new List<Playlist> 
+                {new Playlist("linkin park"){Descripcion = "buena lkista"},
+                new Playlist("skillet"){Descripcion = "buena play"},
+                new Playlist("solence"){ Descripcion = "canerita"}
+                });
+            var mapperConfiguration = new MapperConfiguration(
+                cfg => cfg.AddProfile<Beca.PlaylistInfo.API.Profiles.PlaylistProfile>());
+            var mapper = new Mapper(mapperConfiguration);
+            var logger = Mock.Of<ILogger<Playlist>>();
+            _playlistController = new PlaylistsController(logger,playlistMock.Object,mapper);
+        }
+
+        [Fact]
+        public async Task GetPlaylist_model()
+        {
+            //arrange   
+
+            //Act
+            var result = await _playlistController.GetPlaylists("linkin park", 1,1);
+
+            //Assert.
+            var actionResult = Assert.IsType<ActionResult<IEnumerable<Beca.PlaylistInfo.API.Models.PlaylistWithoutCancionesDto>>>(result);
+            Assert.IsType<OkObjectResult>(actionResult.Result);
+        }
+
+        [Fact]
+        public async Task GetPlaylist_modelDto()
+        {
+            //arrange   
+
+            //Act
+            var result = await _playlistController.GetPlaylists("linkin park", 1, 1);
+
+            //Assert.
+            var actionResult = Assert.IsType<ActionResult<IEnumerable<Beca.PlaylistInfo.API.Models.PlaylistWithoutCancionesDto>>>(result);
+            Assert.IsAssignableFrom<IEnumerable<Beca.PlaylistInfo.API.Models.PlaylistWithoutCancionesDto>>(((OkObjectResult)actionResult.Result).Value);
+        }
+
+        [Fact]
+        public async Task GetPlaylists_ControllerTest()
+        {
+            //arrange   
+            
+            //Act
+
+            var result = await _playlistController.GetPlaylists("linkin park",1,1);
+           
+
+            //Assert.
+            var actionResult = Assert.IsType<ActionResult<IEnumerable<Beca.PlaylistInfo.API.Models.PlaylistWithoutCancionesDto>>>(result);
+            var okObject = Assert.IsType<OkObjectResult>(actionResult.Result);
+            var dtos = Assert.IsAssignableFrom<IEnumerable<Beca.PlaylistInfo.API.Models.PlaylistWithoutCancionesDto>>(okObject.Value);
+            Assert.Equal(3, dtos.Count());
+        }
+
+        public async Task GetPlaylist_ControllerTest()
+        {
+            //Arrage
+
+            /*var playlistMock = new Mock<IPlaylistRepository>();
+            playlistMock
+                .Setup(m => m.GetPlaylistAsync(It.IsAny<int>(), It.IsAny<bool>()))
+                .ReturnsAsync(
+                new Playlist("linking park")
+                );
+            var mapperConfiguration = new MapperConfiguration(
+                cfg => cfg.AddProfile<Beca.PlaylistInfo.API.Profiles.PlaylistProfile>());
+            var mapper = new Mapper(mapperConfiguration);
+            var logger = Mock.Of<ILogger<Playlist>>();
+            var playlistController = new PlaylistsController(logger, playlistMock.Object, mapper);
+            //Act
+            */
+            var result = await _playlistController.GetPlaylist(5,false);
+
+            //Assert
+            var okOkject = Assert.IsType<OkObjectResult>(result);
+            var dtos = Assert.IsAssignableFrom<IEnumerable<Beca.PlaylistInfo.API.Models.PlaylistWithoutCancionesDto>>(okOkject.Value);
+            Assert.Single(dtos);
+
+
+        }
+
+
+    }
+}
